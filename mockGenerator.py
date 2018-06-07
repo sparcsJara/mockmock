b8a3c787f0c6ca87d46519fe3b967f72e7a60d32
jara <jara0701@gmail.com>
2018-06-07 23:54:02 +0900

    implementing getMethods

--- mockGenerator.py
+++ mockGenerator.py
@@ -1,30 +1,66 @@
-import astor
+import astor,ast
 import inspect
+from operator import eq
 
 
 class mockGenerator:
-    def __init__(self, testFile, mockTarget):
+    def __init__(self, sourceCode, testFile, mockTarget):
         self.testFile = testFile
         self.mockTarget = mockTarget
+        self.sourceCode = sourceCode
 
-    def recordMockMethodInfo(self, testFile, mockTarget):
-        ## TODO: 문영
-        self.mockMethodInfo = {
-            'method1': 2,
-            'method2': 1
-        }
+    def recordMockMethodInfo(self):
+        targetMethods = self.getMethods(self.mockTarget)
+        test_file_ast = self.getCorrectTestFileAST() # classDef
+        source_code_ast = self.getCorrectSourceCodeAST() # body = functiondef
+
+        intMethods = []
+        return_list = []
+        for name, isInt in targetMethods.items():
+            if not isInt:
+                continue
+            target_name = name
+            count_of_target = 0
+            for node in ast.walk(test_file_ast):
+                if isinstance(node, ast.Call):
+                    if node.__getattribute__("func").__getattribute__("attr") in intMethods:
+                        count_of_target = count_of_target + 1
+            for node in ast.walk(source_code_ast):
+                if isinstance(node, ast.Call):
+                    if node.__getattribute__("func").__getattribute__("attr") in intMethods:
+                        count_of_target = count_of_target + 1
+            return_list.appen(tuple(target_name, count_of_target))
+
+        self.mockMethodInfo = return_list
+        return return_list
 
     def injectMock(self, testFile, mockTarget, methodCalls):
         testFile = astor.parse_file(testFile)
         print(astor.dump_tree(testFile))
 
-    def getMethods(cs):
-        members = inspect.getmembers(cs, predicate=inspect.ismethod)
-        methods = []
-        for member in members:
-            methods.append(member[0])
-        return methods
+    def getMethods(self):
+        method_dict = {}
+        mock_ast = astor.parse_file(self.mockTarget)
+        classdef_ast = mock_ast.__getattribute__("body")[0]
+        for node in ast.walk(classdef_ast):
+            if isinstance(node, ast.FunctionDef):
+                return_int = False
+                returns = node.__getattribute__("returns")
+                if(isinstance(returns, ast.Name)):
+                    name = returns.__getattribute__("id")
+                    if(eq(name, "int")):
+                        return_int = True
+                method_dict[node.__getattribute__("name")] = return_int
+        return method_dict
 
+    def getMethodType(method, args):
+        return type(method())
+
+    def getCorrectTestFileAST(self):
+        return astor.parse_file(self.testFile)
+
+    def getCorrectSourceCodeAST(self):
+        return astor.parse_file(self.sourceCode)
 
 if __name__ == '__main__':
   gen = mockGenerator('test_cat_owner.py', 'cat_database.py')

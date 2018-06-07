5e84f6542d4fe89d7b476dfc2135d9e744f14841
jara <jara0701@gmail.com>
2018-06-07 20:19:25 +0900

    make get Methods

--- mockGenerator.py
+++ mockGenerator.py
@@ -1,2 +1,17 @@
-class mockGenertor():
-    def readFile(self, filename):
+import inspect
+
+class mockGenerator():
+    testFile
+    mockTarget
+    mockMethodInfo
+
+    def recordMockMethodsInfo(self):
+
+
+
+    def getMethods(cs):
+        members = inspect.getmembers(cs, predicate=inspect.ismethod)
+        methods = []
+        for member in members:
+            methods.append(member[0])
+        return methods
